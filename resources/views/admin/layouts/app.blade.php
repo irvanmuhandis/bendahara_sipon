b95dc6b6457dc8ab937a3f62d51bdfaf2f46bebc
Irvan Muhandis <irvanmuhandis@gmail.com>
2023-08-30 23:36:39 +0700

    ganti judul

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CUAN | Bendahara</title>
+    <title>Keuangan PP KGS</title>
     <link rel="icon" type="image/x-icon" href="{{ url('/images/cuak.ico') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
