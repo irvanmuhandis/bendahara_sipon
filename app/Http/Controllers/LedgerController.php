f2d4bc52daf71df1ddb7525f3c0de6036d004c32
Irvan Muhandis <irvanmuhandis@gmail.com>
2023-10-29 18:42:49 +0700

    ss

--- app/Http/Controllers/LedgerController.php
+++ app/Http/Controllers/LedgerController.php
@@ -190,7 +190,7 @@ class LedgerController extends Controller
             }])
             ->where('payment_status', '<', 3)
             ->whereIn('account_id', $account)
-            ->sum('remainder')->first();
+            ->sum('remainder');
 
         return response()->json([
             'data' => $query,
